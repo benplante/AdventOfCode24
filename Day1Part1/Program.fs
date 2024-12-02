7e68509b59bdf7bd4eb63ae540370b42c24f5433
Ben Plante <ben.plante123@gmail.com>
2024-12-01 22:18:19 -0500

    AdventOfCode24 day 1 init

---
+++ Day1Part1/Program.fs
@@ -0,0 +1,27 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+
+let read parser =
+    Seq.initInfinite (fun _ -> Console.ReadLine())
+    |> Seq.takeWhile (fun s -> not (String.IsNullOrWhiteSpace s))
+    |> Seq.choose parser
+    |> Seq.fold (fun (l1, l2) (i1, i2) -> (i1::l1, i2::l2)) ([], [])
+
+
+let parse (s: string) =
+    let parts = s.Split (" ", StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
+    match parts with
+    | [|a; b|] -> Some (Int32.Parse a, Int32.Parse b)
+    | _ -> None
+
+let left, right = read parse
+
+let result =
+    List.zip (List.sort left) (List.sort right)
+    |> List.map (fun (a, b) -> Math.Abs (a - b))
+    |> List.sum
+
+printfn $"%d{result}"
+
+
---
+++ Day1Part2/Program.fs
@@ -0,0 +1,22 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+
+let read parser =
+    Seq.initInfinite (fun _ -> Console.ReadLine())
+    |> Seq.takeWhile (fun s -> not (String.IsNullOrWhiteSpace s))
+    |> Seq.choose parser
+    |> Seq.fold (fun (l1, l2) (i1, i2) -> (i1::l1, i2::l2)) ([], [])
+
+
+let parse (s: string) =
+    let parts = s.Split (" ", StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
+    match parts with
+    | [|a; b|] -> Some (Int32.Parse a, Int32.Parse b)
+    | _ -> None
+
+let left, right = read parse
+
+left
+|> List.map (fun i -> (right |> List.filter (fun r -> i = r) |> List.length) * i)
+|> List.sum
+|> printfn "%d"
