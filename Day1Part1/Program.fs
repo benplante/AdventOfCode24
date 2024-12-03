46b23d334ea6ab4b181152c1e8447282295e1b6a
Ben Plante <ben.plante123@gmail.com>
2024-12-02 23:55:37 -0500

    day2part2

--- Day1Part1/Program.fs
+++ Day1Part1/Program.fs
@@ -1,6 +1,4 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-
-open System
+﻿open System
 
 let read parser =
     Seq.initInfinite (fun _ -> Console.ReadLine())
--- Day2Part2/Program.fs
+++ Day2Part2/Program.fs
@@ -9,12 +9,20 @@ let isSafe (report: int seq) =
     values |> Seq.forall (function -3 | -2 | -1 -> true | _ -> false) ||
     values |> Seq.forall (function 3 | 2 | 1 -> true | _ -> false)
 
+let compensate (report: int seq) =
+    let samples =
+        report
+        |> Seq.toList
+
+    { 0 .. samples.Length - 1 }
+    |> Seq.exists (fun i -> isSafe <| List.removeAt i samples)
+
 let parse (report: string) =
     report.Split " " |> Seq.map int
 
 Seq.initInfinite (fun _ -> stdin.ReadLine())
 |> Seq.takeWhile (fun s -> not (String.IsNullOrWhiteSpace s))
 |> Seq.map parse
-|> Seq.where isSafe
+|> Seq.where compensate
 |> Seq.length
-|> printf "%d"
+|> printfn "%A"
