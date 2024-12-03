ca1b12ba940f01928fb279b31e43cda3e45a8769
Ben Plante <ben.plante123@gmail.com>
2024-12-02 21:40:31 -0500

    day2part1

---
+++ Day2Part1/Program.fs
@@ -0,0 +1,23 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+
+let isSafe (report: string) =
+    let values =
+        report.Split (" ", StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
+        |> Seq.map Int32.Parse
+        |> Seq.pairwise
+        |> Seq.map (fun (a, b) -> b - a)
+
+
+    values |> Seq.forall (function -3 | -2 | -1 -> true | _ -> false) ||
+    values |> Seq.forall (function 3 | 2 | 1 -> true | _ -> false)
+
+
+
+
+
+Seq.initInfinite (fun _ -> Console.ReadLine())
+|> Seq.takeWhile (fun s -> not (String.IsNullOrWhiteSpace s))
+|> Seq.where isSafe
+|> Seq.length
+|> printf "%d"
