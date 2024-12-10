64afcead3a05bcfdba71a62d0ec5772da0f22113
Ben Plante <ben.plante123@gmail.com>
2024-12-09 22:45:50 -0500

    day3part1,2

---
+++ Day3Part1/Program.fs
@@ -0,0 +1,19 @@
+﻿open System.Text.RegularExpressions
+
+let expr = Regex("mul\((\d+),(\d+)\)")
+
+let matches (expr: Regex) input =
+    let rec matches idx = seq {
+        let m = expr.Match(input, idx)
+        if m.Success then
+            yield m
+            yield! matches (m.Index + m.Length)
+    }
+
+    matches 0
+
+let input = stdin.ReadToEnd()
+matches expr input
+|> Seq.map (fun x -> (int x.Groups[1].Value) * (int x.Groups[2].Value))
+|> Seq.sum
+|> printfn "\n%A"
---
+++ Day3Part2/Program.fs
@@ -0,0 +1,32 @@
+﻿open System.Text.RegularExpressions
+
+let expr = Regex("do\(\)|don't\(\)|mul\((\d+),(\d+)\)")
+
+let matches (expr: Regex) input =
+    let rec matches idx = seq {
+        let m = expr.Match(input, idx)
+        if m.Success then
+            yield m
+            yield! matches (m.Index + m.Length)
+    }
+
+    matches 0
+
+let folder (state: int * bool) (item: Match) =
+    let result, enabled = state
+    match item.Value with
+    | "do()" -> result, true
+    | "don't()" -> result, false
+    | _ when not enabled -> state
+    | _ ->
+        let res = (int item.Groups[1].Value) * (int item.Groups[2].Value)
+        res + result, enabled
+
+
+let input = stdin.ReadToEnd()
+matches expr input
+|> Seq.fold folder (0, true)
+|> printfn "\n%A"
+// expr.EnumerateMatches(input)
+// |> Seq.cast<ValueMatch>
+// |> Seq.iter (printfn "%A")
