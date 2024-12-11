ed07a50297e5bc2c3c05d5ede36e61a5c470dd29
Ben Plante <ben.plante123@gmail.com>
2024-12-10 23:27:28 -0500

    day4part1

---
+++ Day4Part1/Program.fs
@@ -0,0 +1,36 @@
+﻿open System
+
+let grid =
+    Seq.initInfinite (fun _ -> stdin.ReadLine())
+    |> Seq.takeWhile (fun s -> not (String.IsNullOrWhiteSpace s))
+    |> array2D
+
+let word = "XMAS"
+
+let stride = [-1; 0; 1]
+let steps =
+    stride
+    |> List.collect (fun s -> stride |> List.map(fun s' -> (s, s')))
+    |> List.filter (fun s -> not (s = (0, 0)))
+
+let search (i: int) (j: int) _=
+    let walk ((di, dj): int * int) =
+        let rec walk i' j' n =
+            if n = word.Length then 1
+            else if (i' < 0 || i' >= Array2D.length1 grid) || (j' < 0 || j' >= Array2D.length2 grid) then 0
+            else if word[n] = grid[i', j'] then
+                walk (i' + di) (j' + dj) (n + 1)
+            else 0
+
+        walk i j 0
+
+    steps
+    |> Seq.map walk
+    |> Seq.sum
+
+
+grid
+|> Array2D.mapi search
+|> Seq.cast<int>
+|> Seq.sum
+|> printfn "%A"
